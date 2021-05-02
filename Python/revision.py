8cde1721447f37d74f58398f181c52a55fbdf76b
dzpiers <dhruv.piers@gmail.com>
2021-05-02 14:43:35 +1000

    other stuff

---
+++ Python/revision.py
@@ -0,0 +1,13 @@
+## Copy code here
+print(2**10000)
+
+print("Python"[-3])
+
+def foo(x, y):
+    z = bar(y)
+    x = 2 * z
+    return x
+def bar(w):
+    return w ** 2
+goo = foo(11, 88)
+print(goo)
